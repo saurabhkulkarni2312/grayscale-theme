ca5005bcc5dfd06e65d516d616fc8ea83c9b5c69
saurabhkulkarni2312 <saurabhkulkarni2312@gmail.com>
2016-10-01 15:15:56 -0700

    About.html, Contact.html and greyscale.js edited

--- js/grayscale.js
+++ js/grayscale.js
@@ -47,7 +47,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(32.8815, -117.2355), // UCSD
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
